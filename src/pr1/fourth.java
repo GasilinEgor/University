a4e5a5b7002c55602d462032dd1cac289989996d
Egor <eg.gasilin2018@yandex.ru>
2023-09-13 12:28:24 +0300

    1.4

---
+++ src/pr1/fourth.java
@@ -0,0 +1,11 @@
+package pr1;
+
+public class fourth {
+    public static void main(String[] args) {
+        System.out.print("1 ");
+        for (int i = 2; i <= 10; i++)
+        {
+            System.out.print("1/" + Integer.toString(i) + " ");
+        }
+    }
+}
