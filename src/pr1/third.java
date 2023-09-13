b146a546f76478d8d8116ae02729bb891c776d45
Egor <eg.gasilin2018@yandex.ru>
2023-09-13 12:21:34 +0300

    1.3

---
+++ src/pr1/third.java
@@ -0,0 +1,10 @@
+package pr1;
+
+public class third {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++)
+        {
+            System.out.println(args[i]);
+        }
+    }
+}
