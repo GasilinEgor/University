7c784ed10ea1872265203fc58ea7e84d1f9dd402
Egor <eg.gasilin2018@yandex.ru>
2023-09-25 13:49:05 +0300

    5.2

---
+++ src/pr5/Second/Square.java
@@ -0,0 +1,15 @@
+package pr5.Second;
+
+import java.awt.*;
+
+public class Square extends Rectangle {
+    public Square(int x, int y, Color color, int side) {
+        super(x, y, color, side, side);
+    }
+
+
+    @Override
+    void Draw(Graphics g) {
+        super.Draw(g);
+    }
+}
