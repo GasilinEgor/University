4efa3deada0144b7ab5624bddc84c91de90ac416
Egor <eg.gasilin2018@yandex.ru>
2023-10-02 13:47:41 +0300

    10.2

--- src/pr10/Main.java
+++ src/pr10/Main.java
@@ -1,10 +1,23 @@
 package pr10;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Student[] iDNumber = new Student[0];
-        iDNumber = setArray(iDNumber);
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            iDNumber = setArray(iDNumber);
+        }
+        System.out.println("Если хотите отсортировать по среднему баллу, введите 1, если по фамилии - 2");
+        int change = scanner.nextInt();
+        if (change == 1) {
+            MergeSort(iDNumber, new StudentComparator(1));
+        }
+        else {
+            MergeSort(iDNumber, new StudentComparator(2));
+        }
         outArray(iDNumber);
     }
 
@@ -22,7 +35,7 @@ public class Main {
 
     public static void outArray(Student[] students) {
         for (Student student : students) {
-            System.out.println("Студент " + student.getSurname() + " " + student.getName());
+            System.out.println("\nСтудент " + student.getSurname() + " " + student.getName());
             System.out.println("Должность: " + student.getSpecialization());
             System.out.println(student.getYear() + " Курс, Группа " + student.getGroup());
             System.out.println("Средний балл: " + student.getMediumScores());
@@ -30,5 +43,40 @@ public class Main {
     }
 
 
-    public static void MergeSort() {}
+    public static void MergeSort(Student[] list, Comparator<Student> comparator) {
+        if (list.length <= 1) {
+            return;
+        }
+        Student[] arr1 = Arrays.copyOfRange(list, 0, list.length / 2), arr2 = Arrays.copyOfRange(list, list.length / 2, list.length);
+        MergeSort(arr1, comparator);
+        MergeSort(arr2, comparator);
+
+        merge(list, arr1, arr2, comparator);
+    }
+
+
+    public static void merge(Student[] list, Student[] arr1, Student[] arr2, Comparator<Student> comparator) {
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if ((comparator.compare(arr1[i], arr2[j])) > 0) {
+                list[k] = arr1[i];
+                i++;
+            }
+            else {
+                list[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < arr1.length) {
+            list[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < arr2.length) {
+            list[k] = arr2[j];
+            j++;
+            k++;
+        }
+    }
 }
